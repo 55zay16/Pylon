597f913d0ef919a1ee18427a339ee9a566569f97
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-08 13:31:44 -0500

    whole gui lib

--- modules/UIlib.lua
+++ modules/UIlib.lua
@@ -23,7 +23,7 @@ function UIlib:InitWindow(Title)
     Main.BorderSizePixel = 0
     Main.Position = UDim2.new(0.305642635, 0, 0.198492467, 0)
     Main.Size = UDim2.new(0.324973881, 0, 0.0402010046, 0)
-    Main.Font = Enum.Font.Unknown
+    Main.Font = Enum.Font.RobotoMono
     Main.Text = Title
     Main.TextColor3 = Color3.fromRGB(171, 227, 255)
     Main.TextScaled = true
@@ -158,10 +158,10 @@ function UIlib:InitWindow(Title)
         Minimize.MouseButton1Click:Connect(function()
             if Minimize.Rotation == 0 then
                 TweenService:Create(Minimize, TweenInfo.new(0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {Rotation = 180}):Play()
-                Dropdown:TweenSize(UDim2.new(0.999818683, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 0.3, true)
+                Dropdown:TweenSize(UDim2.new(1.00665498, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 0.3, true)
             elseif Minimize.Rotation == 180 then
                 TweenService:Create(Minimize, TweenInfo.new(0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {Rotation = 0}):Play()
-                Dropdown:TweenSize(UDim2.new(0.999818683, 0, 0.104166664, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 0.3, true)
+                Dropdown:TweenSize(UDim2.new(1.00665498, 0, 12, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Sine, 0.3, true)
             end
         end)
 
@@ -169,6 +169,257 @@ function UIlib:InitWindow(Title)
             gui:Destroy()
         end)
     end)()
+
+    return Pylon
+end
+
+function UIlib:CreateTab(PylonInstance, Title)
+    local TabName = Instance.new("Frame")
+    local Buttons = Instance.new("ScrollingFrame")
+    local UIListLayout = Instance.new("UIListLayout")
+    local Settings = Instance.new("ScrollingFrame")
+    local UIListLayout_2 = Instance.new("UIListLayout")
+    
+    TabName.Name = Title
+    TabName.Parent = PylonInstance.Main.Dropdown
+    TabName.BackgroundColor3 = Color3.fromRGB(29, 38, 43)
+    TabName.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    TabName.BorderSizePixel = 0
+    TabName.Visible = false
+    TabName.Position = UDim2.new(0, 0, 0.104166664, 0)
+    TabName.Size = UDim2.new(0.641362786, 0, 0.895833313, 0)
+    
+    Buttons.Name = "Buttons"
+    Buttons.Parent = TabName
+    Buttons.Active = true
+    Buttons.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    Buttons.BackgroundTransparency = 1.000
+    Buttons.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Buttons.BorderSizePixel = 0
+    Buttons.Size = UDim2.new(1, 0, 1, 0)
+    Buttons.ScrollBarThickness = 4
+    
+    UIListLayout.Parent = Buttons
+    UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    
+    Settings.Name = "Settings"
+    Settings.Parent = TabName
+    Settings.Active = true
+    Settings.BackgroundColor3 = Color3.fromRGB(36, 49, 54)
+    Settings.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Settings.BorderSizePixel = 0
+    Settings.Position = UDim2.new(1, 0, 0, 0)
+    Settings.Size = UDim2.new(0.558897257, 0, 1, 0)
+    Settings.ScrollBarThickness = 4
+    Settings.AutomaticCanvasSize = Enum.AutomaticSize.Y
+    Settings.ScrollingDirection = Enum.ScrollingDirection.Y
+    
+    UIListLayout_2.Parent = Settings
+    UIListLayout_2.FillDirection = Enum.FillDirection.Vertical
+    UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+    UIListLayout_2.VerticalAlignment = Enum.VerticalAlignment.Top
+    
+    local TabButton = Instance.new("TextButton")
+
+    TabButton.Name = "TabButton"
+    TabButton.Parent = PylonInstance.Main.Dropdown.Tabs
+    TabButton.BackgroundColor3 = Color3.fromRGB(20, 27, 30)
+    TabButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    TabButton.BorderSizePixel = 0
+    TabButton.Position = UDim2.new(0.305642635, 0, 0.238806695, 0)
+    TabButton.Size = UDim2.new(0.194000006, 0, 0.050999999, 0)
+    TabButton.Font = Enum.Font.RobotoMono
+    TabButton.Text = Title
+    TabButton.TextColor3 = Color3.fromRGB(173, 226, 255)
+    TabButton.TextScaled = true
+    TabButton.TextSize = 14.000
+    TabButton.TextWrapped = true
+
+    
+    TabButton.MouseButton1Click:Connect(function()
+        for _, Item in pairs(PylonInstance.Main.Dropdown:GetChildren()) do
+            if Item.Name == "Tabs" or Item.Name == "Border" then
+            else
+                Item.Visible = false
+            end
+        end
+
+        for _, Item in pairs(PylonInstance.Main.Dropdown.Tabs:GetChildren()) do
+            if Item:IsA("TextButton") then
+                Item.BackgroundColor3 = Color3.fromRGB(20, 27, 30)
+            end
+        end
+
+        TabName.Visible = true
+        TabButton.BackgroundColor3 = Color3.fromRGB(46, 61, 68)
+    end)
+
+    return TabName
+end
+
+function UIlib:CreateButton(Title,TabInstance,ButtonSettings,Callback)
+    local Button = Instance.new("TextButton")
+    local UIPadding = Instance.new("UIPadding")
+    local SettingsButton = Instance.new("TextButton")
+    local UIPadding_2 = Instance.new("UIPadding")
+
+    Button.Name = Title
+    Button.Parent = TabInstance.Buttons
+    Button.BackgroundColor3 = Color3.fromRGB(20, 27, 30)
+    Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Button.BorderSizePixel = 0
+    Button.Size = UDim2.new(1, 0, 0.0469024193, 0)
+    Button.Font = Enum.Font.RobotoMono
+    Button.Text = Title
+    Button.TextColor3 = Color3.fromRGB(173, 226, 255)
+    Button.TextScaled = true
+    Button.TextSize = 14.000
+    Button.TextWrapped = true
+    Button.TextXAlignment = Enum.TextXAlignment.Left
+
+    UIPadding.Parent = Button
+    UIPadding.PaddingLeft = UDim.new(0, 3)
+
+    SettingsButton.Name = "SettingsButton"
+    SettingsButton.Parent = Button
+    SettingsButton.BackgroundColor3 = Color3.fromRGB(25, 34, 38)
+    SettingsButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    SettingsButton.BorderSizePixel = 0
+    SettingsButton.Position = UDim2.new(0.76262629, 0, -0.0314491875, 0)
+    SettingsButton.Size = UDim2.new(0.237373739, 0, 1.0314486, 0)
+    SettingsButton.Font = Enum.Font.RobotoMono
+    SettingsButton.Text = "Settings"
+    SettingsButton.TextColor3 = Color3.fromRGB(173, 226, 255)
+    SettingsButton.TextScaled = true
+    SettingsButton.TextSize = 14.000
+    SettingsButton.TextWrapped = true
+    SettingsButton.TextXAlignment = Enum.TextXAlignment.Left
+
+    UIPadding_2.Parent = SettingsButton
+    UIPadding_2.PaddingLeft = UDim.new(0, 3)
+
+    for _, Item in pairs(ButtonSettings) do
+        if Item["Type"] == "Label" then
+            local LabelTabname = Instance.new("TextLabel")
+
+            LabelTabname.Name = Button.Name
+            LabelTabname.Parent = TabInstance.Settings
+            LabelTabname.BackgroundColor3 = Color3.fromRGB(28, 38, 42)
+            LabelTabname.BorderColor3 = Color3.fromRGB(0, 0, 0)
+            LabelTabname.BorderSizePixel = 0
+            LabelTabname.Position = UDim2.new(0, 0, 4.39155805e-08, 0)
+            LabelTabname.Size = UDim2.new(0, 223, 0, 26)
+            LabelTabname.Font = Enum.Font.Unknown
+            LabelTabname.TextColor3 = Color3.fromRGB(171, 227, 255)
+            LabelTabname.TextScaled = true
+            LabelTabname.TextSize = 14.000
+            LabelTabname.Visible = false
+            LabelTabname.Text = Item["Text"]
+            LabelTabname.TextWrapped = true
+            Item["Object"] = LabelTabname
+        elseif Item["Type"] == "TextBox" then
+            local TextBoxTabname = Instance.new("TextBox")
+            TextBoxTabname.Name = Button.Name
+            TextBoxTabname.Parent = TabInstance.Settings
+            TextBoxTabname.BackgroundColor3 = Color3.fromRGB(25, 34, 38)
+            TextBoxTabname.BorderColor3 = Color3.fromRGB(0, 0, 0)
+            TextBoxTabname.BorderSizePixel = 0
+            TextBoxTabname.Position = UDim2.new(0, 0, 0.0748293847, 0)
+            TextBoxTabname.Size = UDim2.new(0, 223, 0, 22)
+            TextBoxTabname.Font = Enum.Font.Unknown
+            TextBoxTabname.PlaceholderText = Item["Placeholder"]
+            TextBoxTabname.TextColor3 = Color3.fromRGB(171, 227, 255)
+            TextBoxTabname.TextScaled = true
+            TextBoxTabname.TextSize = 14.000
+            TextBoxTabname.Visible = false
+            TextBoxTabname.TextWrapped = true
+            Item["Object"] = TextBoxTabname
+
+            TextBoxTabname:GetPropertyChangedSignal("Text"):Connect(function()
+                Item["Value"] =TextBoxTabname.Text
+            end)
+        elseif Item["Type"] == "Button" then
+            local ButtonTabname = Instance.new("TextButton")
+            local UIPadding = Instance.new("UIPadding")
+            
+            ButtonTabname.Name = Button.Name
+            ButtonTabname.Parent = TabInstance.Settings
+            ButtonTabname.BackgroundColor3 = Color3.fromRGB(25, 34, 38)
+            ButtonTabname.BorderColor3 = Color3.fromRGB(0, 0, 0)
+            ButtonTabname.BorderSizePixel = 0
+            ButtonTabname.Position = UDim2.new(0, 0, 0.0748293847, 0)
+            ButtonTabname.Size = UDim2.new(1.00000024, 0, 0.090325743, 0)
+            ButtonTabname.Font = Enum.Font.Unknown
+            ButtonTabname.TextColor3 = Color3.fromRGB(173, 226, 255)
+            ButtonTabname.TextScaled = true
+            ButtonTabname.TextSize = 14.000
+            ButtonTabname.Text = Item["Text"]
+            ButtonTabname.Visible = false
+            ButtonTabname.TextWrapped = true
+            
+            UIPadding.Parent = ButtonTabname
+            UIPadding.PaddingLeft = UDim.new(0, 3)
+
+            Item["Object"] = ButtonTabname
+
+            ButtonTabname.MouseButton1Click:Connect(function()
+                Item["Callback"](ButtonTabname)
+            end)
+        elseif Item["Type"] == "ToggleButton" then
+            local ButtonTabname = Instance.new("TextButton")
+            local UIPadding = Instance.new("UIPadding")
+            
+            ButtonTabname.Name = Button.Name
+            ButtonTabname.Parent = TabInstance.Settings
+            ButtonTabname.BackgroundColor3 = Color3.fromRGB(25, 34, 38)
+            ButtonTabname.BorderColor3 = Color3.fromRGB(0, 0, 0)
+            ButtonTabname.BorderSizePixel = 0
+            ButtonTabname.Position = UDim2.new(0, 0, 0.0748293847, 0)
+            ButtonTabname.Size = UDim2.new(1.00000024, 0, 0.090325743, 0)
+            ButtonTabname.Font = Enum.Font.Unknown
+            ButtonTabname.TextColor3 = Color3.fromRGB(173, 226, 255)
+            ButtonTabname.TextScaled = true
+            ButtonTabname.TextSize = 14.000
+            ButtonTabname.Text = Item["Text"]
+            ButtonTabname.Visible = false
+            ButtonTabname.TextWrapped = true
+            
+            UIPadding.Parent = ButtonTabname
+            UIPadding.PaddingLeft = UDim.new(0, 3)
+
+            Item["Object"] = ButtonTabname
+
+            ButtonTabname.MouseButton1Click:Connect(function()
+                if ButtonTabname.BackgroundColor3 == Color3.fromRGB(25, 34, 38) then
+                    ButtonTabname.BackgroundColor3 = Color3.fromRGB(46, 61, 68)
+                    Item["Callback"](true)
+                else
+                    ButtonTabname.BackgroundColor3 = Color3.fromRGB(25, 34, 38)
+                    Item["Callback"](false)
+                end
+            end)
+        end
+    end
+
+    Button.MouseButton1Down:Connect(function()
+        if Button.BackgroundColor3 == Color3.fromRGB(20, 27, 30) then
+            Button.BackgroundColor3 = Color3.fromRGB(46, 61, 68)
+            Callback(true)
+        else
+            Button.BackgroundColor3 = Color3.fromRGB(20, 27, 30)
+            Callback(false)
+        end
+    end)
+
+    SettingsButton.MouseButton1Down:Connect(function()
+        for _, Item in pairs(TabInstance.Settings:GetChildren()) do
+            if Item:IsA("UIListLayout") == false and Item.Name == Button.Name then
+                Item.Visible = true
+            elseif Item:IsA("UIListLayout") == false then
+                Item.Visible = false
+            end
+        end
+    end)
 end
 
 return UIlib
