c6a7eb348e3db5005bb11a2b483fb0783e231664
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-08 13:33:53 -0500

    Update Core.lua

--- modules/Core.lua
+++ modules/Core.lua
@@ -1,4 +1,5 @@
 local Core = {}
 Core.UIlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/modules/UIlib.lua"))()
+Core.Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/modules/Notifications.lua"))()
 
 return Core
