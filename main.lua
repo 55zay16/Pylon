805ac7cd6c8ca514d766abe95ca6744366dfeeaf
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-09 16:59:35 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
-warn("Not support yet")
+if game.GameId == 4124008017 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/Bridge%20Duel.lua"))()
+end
