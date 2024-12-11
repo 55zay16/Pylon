541b91bdd98a3a23b448baa2d3cfd8967081bd88
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-11 17:33:40 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 if game.GameId == 4124008017 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/Bridge%20Duel.lua"))()
+elseif game.GameId == 3258873704 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/Skywars.lua"))()
 end
