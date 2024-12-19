75afed130bac1884d6116b0b6f546a2f4f19b3ab
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-19 16:09:32 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ if game.GameId == 4124008017 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/Bridge%20Duel.lua"))()
 elseif game.GameId == 3258873704 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/Skywars.lua"))()
+elseif game.GameId == 3808081382 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/scripts/The%20Strongest%20Battlegrounds.lua"))()
 end
