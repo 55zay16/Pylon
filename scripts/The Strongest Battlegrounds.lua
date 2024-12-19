99ac9a97ebfe356e9aca2cc290dc75f00eca05b2
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-12-19 16:10:27 -0500

    Update The Strongest Battlegrounds.lua

--- scripts/The Strongest Battlegrounds.lua
+++ scripts/The Strongest Battlegrounds.lua
@@ -1,1 +1,285 @@
+local Core = loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Pylon/refs/heads/main/modules/Core.lua"))()
+
+local Pylon = Core.UIlib:InitWindow("Pylon")
+
+local Combat = Core.UIlib:CreateTab(Pylon, "Combat")
+local Movement = Core.UIlib:CreateTab(Pylon, "Movement")
+local Visuals = Core.UIlib:CreateTab(Pylon, "Visuals")
+local Utility = Core.UIlib:CreateTab(Pylon, "Utility")
+
+local lplr = game:GetService("Players").LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+
+local SpeedSettings = { 
+    ["Speed Label"] = {
+        ["Object"] = nil,
+        ["Type"] = "Label",
+        ["Text"] = "Speed",
+    },
+    ["Speed"] = {
+        ["Object"] = nil,
+        ["Type"] = "TextBox",
+        ["Placeholder"] = "speed 0.5",
+        ["Value"] = 0.5,
+    },
+}
+
+
+local CANAUTOFARM = false
+local AutoFarm = Core.UIlib:CreateButton(Combat, "Auto Farm",{},function(NewStatus)
+    CANAUTOFARM = NewStatus
+end)
+
+local TPAURASettings = {
+    ["Range Label"] = {
+        ["Object"] = nil,
+        ["Type"] = "Label",
+        ["Text"] = "Range",
+    },
+    ["Range"] = {
+        ["Object"] = nil,
+        ["Type"] = "TextBox",
+        ["Placeholder"] = "Attack Range (20)",
+        ["Value"] = 20,
+    },
+    ["Anti Death"] = {
+        ["Object"] = nil,
+        ["Type"] = "ToggleButton",
+        ["Text"] = "Anti Death",
+        ["Callback"] = nil,
+        ["Value"] = false,
+    },
+    ["Anti Death Health Threshold"] = {
+        ["Object"] = nil,
+        ["Type"] = "TextBox",
+        ["Placeholder"] = "Health Threshold (20)",
+        ["Value"] = 20,
+    },
+}
+local CANTPAURA = false
+local TPAURA = Core.UIlib:CreateButton(Combat, "Tp Aura",TPAURASettings,function(NewStatus)
+    CANTPAURA = NewStatus
+end)
+
+local CanSpeed = false
+local Speed = Core.UIlib:CreateButton(Movement, "Speed",SpeedSettings,function(NewStatus)
+    CanSpeed = NewStatus
+end)
+
+local CanInfinteJump = false
+local InfiniteJump = Core.UIlib:CreateButton(Movement, "Infinite Jump",{},function(NewStatus)
+    CanInfinteJump = NewStatus
+end)
+
+local CANESP = false
+local ESP = Core.UIlib:CreateButton(Visuals, "ESP",{},function(NewStatus)
+    CANESP = NewStatus
+
+    if CANESP == true then
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= lplr then
+                pcall(function()
+                    local Highlight = Instance.new("Highlight")
+                    Highlight.FillColor = Color3.fromRGB(255,255,255)
+                    Highlight.OutlineColor = Color3.fromRGB(255,255,255)
+                    Highlight.FillTransparency = 0.3
+                    Highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+                    Highlight.Enabled = true
+                    Highlight.Parent = v.Character
+                end)
+            end
+        end
+    else
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v.Character:FindFirstChild("Highlight") then
+                pcall(function()
+                    v.Character.Highlight:Destroy()
+                end)
+            end
+        end
+    end
+end)
+
+local CANTRACERS = false
+local TRACERS = Core.UIlib:CreateButton(Visuals, "Tracers",{},function(NewStatus)
+    CANTRACERS = NewStatus
+
+    if CANTRACERS == true then
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= lplr then
+                pcall(function()
+                    local Tracer = Instance.new("Beam")
+                    Tracer.Color = ColorSequence.new(Color3.fromRGB(0, 0, 0))
+                    Tracer.Width0 = 0.1
+                    Tracer.Width1 = 0.1
+                    Tracer.Attachment0 = lplr.Character.HumanoidRootPart.RootAttachment
+                    Tracer.Attachment1 = v.Character.HumanoidRootPart.RootAttachment
+                    Tracer.Parent = v.Character.Head
+                end) 
+            end
+        end
+    else
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v.Character.Head:FindFirstChild("Beam") then
+                pcall(function()
+                    v.Character.Head.Beam:Destroy()
+                end)
+            end
+        end
+    end
+end)
+
+local CANNAMETAGS = false
+local NAMETAGS = Core.UIlib:CreateButton(Visuals, "Nametags",{},function(NewStatus) 
+    CANNAMETAGS = NewStatus
+
+    if CANNAMETAGS == true then
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v ~= lplr then
+                pcall(function()
+                    local Nametag = Instance.new("BillboardGui")
+                    Nametag.Name = "Nametag"
+                    Nametag.Size = UDim2.new(0, 100, 0, 40)
+                    Nametag.AlwaysOnTop = true
+                    Nametag.StudsOffset = Vector3.new(0, 2, 0)
+                    Nametag.Parent = v.Character.Head
+
+                    local TextLabel = Instance.new("TextLabel")
+                    TextLabel.BackgroundTransparency = 1
+                    TextLabel.Size = UDim2.new(1, 0, 1, 0)
+                    TextLabel.Text = v.Name
+                    TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+                    TextLabel.TextSize = 14.000
+                    TextLabel.Parent = Nametag
+                    TextLabel.Font = Enum.Font.RobotoMono
+                end)
+            end
+        end
+    else
+        for _,v in pairs(game:GetService("Players"):GetPlayers()) do
+            if v.Character.Head:FindFirstChild("Nametag") then
+                pcall(function()
+                    v.Character.Head.Nametag:Destroy()
+                end)
+            end
+        end
+    end
+end)
+
+game.Players.PlayerAdded:connect(function(Player)
+    Player.CharacterAdded:Connect(function(Character)
+        task.wait(0.3)
+        if CANESP == true then
+            local Highlight = Instance.new("Highlight")
+            Highlight.FillColor = Color3.fromRGB(255,255,255)
+            Highlight.OutlineColor = Color3.fromRGB(255,255,255)
+            Highlight.FillTransparency = 0.3
+            Highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+            Highlight.Enabled = true
+            Highlight.Parent = Character
+        end
+
+        if CANTRACERS == true then
+            local Tracer = Instance.new("Beam")
+            Tracer.Color = ColorSequence.new(Color3.fromRGB(0, 0, 0))
+            Tracer.Width0 = 0.1
+            Tracer.Width1 = 0.1
+            Tracer.Attachment0 = lplr.Character.HumanoidRootPart.RootAttachment
+            Tracer.Attachment1 = Character.HumanoidRootPart.RootAttachment
+            Tracer.Parent = Character.Head
+        end
+
+        if CANNAMETAGS == true then
+            local Nametag = Instance.new("BillboardGui")
+            Nametag.Name = "Nametag"
+            Nametag.Size = UDim2.new(0, 100, 0, 40)
+            Nametag.AlwaysOnTop = true
+            Nametag.StudsOffset = Vector3.new(0, 2, 0)
+            Nametag.Parent = Character.Head
+
+            local TextLabel = Instance.new("TextLabel")
+            TextLabel.BackgroundTransparency = 1
+            TextLabel.Size = UDim2.new(1, 0, 1, 0)
+            TextLabel.Text = Player.Name
+            TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.TextSize = 14.000
+            TextLabel.Parent = Nametag
+            TextLabel.Font = Enum.Font.RobotoMono
+        end
+    end)
+end)
+
+UserInputService.JumpRequest:Connect(function()
+    if CanInfinteJump == true then
+        lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+    end
+end)
+
+coroutine.wrap(function()
+    while task.wait() do
+        local s,e = pcall(function()
+            if CanSpeed == true then
+                lplr.Character.HumanoidRootPart.CFrame += lplr.Character.Humanoid.MoveDirection * tonumber(SpeedSettings["Speed"]["Value"])
+            end
+        end)
+
+        if s == false then
+            print(e)
+        end
+    end 
+end)()
+
+
+local function GetNearestPlayer(Range)
+    local Distance = math.huge
+    local Player = nil
+
+    for _,v in pairs(game:GetService("Players"):GetChildren()) do
+        if v ~= lplr then
+            local Magnitude = (v.Character.PrimaryPart.Position - lplr.Character.PrimaryPart.Position).magnitude
+
+            if Magnitude < Distance and Magnitude <= Range then
+                Distance = Magnitude
+                Player = v
+            end
+        end
+    end
+
+    return Player
+end
+
+coroutine.wrap(function()
+    while task.wait() do
+        local s,e = pcall(function()
+            if CANAUTOFARM == true then
+                local TargetPlayer = GetNearestPlayer(100000)
+
+                if TargetPlayer then
+                    lplr.Character.HumanoidRootPart.CFrame = TargetPlayer.Character.HumanoidRootPart.CFrame - TargetPlayer.Character.HumanoidRootPart.CFrame.LookVector * 4
+                end
+            end
+
+            if CANTPAURA == true then
+                local TargetPlayer = GetNearestPlayer(tonumber(TPAURASettings["Range"]["Value"]))
+
+                if TargetPlayer then
+                    if TPAURASettings["Anti Death"]["Value"] == true then
+                        if lplr.Character.Humanoid.Health <= tonumber(TPAURASettings["Anti Death Health Threshold"]["Value"]) then
+                            lplr.Character.HumanoidRootPart.CFrame = TargetPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0,20,0)
+                            lplr.Character.Humnaoid.Jump = true
+                        else
+                            lplr.Character.HumanoidRootPart.CFrame = TargetPlayer.Character.HumanoidRootPart.CFrame  - TargetPlayer.Character.HumanoidRootPart.CFrame.LookVector * 4
+                        end
+                    else
+                        lplr.Character.HumanoidRootPart.CFrame = TargetPlayer.Character.HumanoidRootPart.CFrame - TargetPlayer.Character.HumanoidRootPart.CFrame.LookVector * 4
+                    end
+                end
+            end
+        end)
+
+        if s == false then
+            print(e)
+        end
+    end
+end)()
 
